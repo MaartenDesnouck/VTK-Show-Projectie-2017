f27bb572141f957174a2b1e6d1f3495cc81cfc98
Maarten Desnouck <maarten.desnouck@gmail.com>
2017-03-20 20:54:11 +0100

    trow away a lot of stuff

--- public/js/show.js
+++ public/js/show.js
@@ -4,7 +4,7 @@ var sectie = 0
 document.onkeypress = function kiesSectie() {
     switch (event.keyCode) {
         case 113:
-            //console.log("Previous");
+            // S - previous
             sectie--;
             if (sectie < 0) {
                 sectie = 0;
@@ -12,27 +12,19 @@ document.onkeypress = function kiesSectie() {
             render(sectie, 'back');
             break;
         case 100:
-            //console.log("Next");
+            // W - next
             sectie++;
             render(sectie, 'forward');
             break;
         case 32:
+            // SPACE BAR - pause/resume video
             var video = document.getElementsByTagName('video')[0]
             video.paused ? video.play() : video.pause();
             break;
     }
 }
 
-//TINDER
-function like(liked){
-    var animationEndEvent = "webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend";
-    var animate = liked ? 'animateYes' : 'animateNo';
-    $('.person').eq(0).addClass(animate).one(animationEndEvent, function() {
-        $(this).remove();
-    });
-}
-
-//CONTENT
+// CONTENT
 function fadeIn() {
     $('#content').animate({
         opacity: 1.0,
@@ -79,7 +71,6 @@ function render(sectie, direction) {
             // ****************** INTRO ******************
             //gewoon het promofilmpje (filmpje)
             //efkes zwart
-            //dansje hier ergens met een zekere achtergrond
 
         case 2: //overgang naar het introfilmpje en daarna terug zwart
             //TODO change trailer file
@@ -92,6 +83,7 @@ function render(sectie, direction) {
 
             //einde filmpje detecteren en dan fadeout
             document.getElementById('trailer').addEventListener('ended', myHandler_trailer, false);
+
             function myHandler_trailer(e) {
                 fadeOut();
             }
@@ -104,26 +96,27 @@ function render(sectie, direction) {
             // laat de lantaarnpalen één voor één uit gaan.
             // Wat later vliegt Hagrid over de huisjes en laat baby Harry uit de lucht vallen (zwarte silhouet).
             // Eventueel alles in vectorgrafics, te bespreken
+            //TODO find pic
+            //TODO animate this shit
+            setContent("<center><img src=/img/straat.png alt='straat' style='width: 100vw; height: 100vh;'></center>")
+            fadeIn()
+            break;
 
         case 4:
             // ACHTERGROND WOONKAMER HILOKKERS
-            $('#content').animate({
-                opacity: 0.0,
-            }, 2000, function() {
-                setContent(
-                    "<video muted autoplay loop id='00173'>" +
-                    "<source src='/video/00173.mp4' type='video/mp4'>" +
-                    "</video>"
-                )
-                fadeIn()
-            });
+            //TODO find pic
+            fadeOut()
+            setContent("<center><img src=/img/woonkamer_hilok.png alt='aeyels' style='width: 100vw; height: 100vh;'></center>")
+            fadeIn()
             break;
+
         case 5:
             // HAGRID ZIJN LIEDJE
             // Wolken die voorbij gaan.
             // Beeld van de Wegisweg
-            #TODO
-            setContent("<center><img src=/img/aeyels2.png alt='aeyels' style='width: 100vw; height: 100vh;'></center>")
+            //TODO find pic
+            //TODO animate this shit of video?
+            setContent("<center><img src=/img/wegisweg.png alt='wegisweg' style='width: 100vw; height: 100vh;'></center>")
             fadeIn()
             break;
         case 6:
@@ -133,13 +126,15 @@ function render(sectie, direction) {
             // Gewoon een tramperron van Gent Sint Pieters.
             // Linkerhelft en rechterhelft zouden twee verschillende perrons moeten
             // zijn gescheiden worden door een muur waar Harry, Ron en Hermelien doorlopen
-            setContent("<center><img src=/img/aeyels2.png alt='aeyels' style='width: 100vw; height: 100vh;'></center>")
+            //TODO take pic
+            setContent("<center><img src=/img/perron.png alt='perron' style='width: 100vw; height: 100vh;'></center>")
             fadeIn()
             break;
 
         case 8:
             // FILMPJE HARRY, RON EN HERMELIEN PAKKEN DE TRAM NAAR ZWIJNAARDE
             // Does what it says on the tin. Gaat opgenomen worden op de filmdag
+            //TODO get video
             setContent(
                 "<video autoplay id='tram'>" +
                 "<source src='/video/tram.mp4' type='video/mp4'>" +
@@ -147,8 +142,9 @@ function render(sectie, direction) {
             )
             fadeIn()
 
-            //einde filmpje detecteren en dan fadeout
+            // einde filmpje detecteren en dan fadeout
             document.getElementById('tram').addEventListener('ended', myHandler_trailer, false);
+
             function myHandler_trailer(e) {
                 fadeOut();
             }
@@ -157,13 +153,14 @@ function render(sectie, direction) {
         case 9:
             // ACHTERGROND LERAARSKAMER ZWIJNAARDE
             // Ergens een leuke oude zaal vinden met wat zetels
+            //TODO find pic
             setContent("<center><img src=/img/leraarskamer.png alt='leraarskamer' style='width: 100vw; height: 100vh;'></center>")
             fadeIn()
             break;
 
         case 10:
             // WAT HAGRID IN DE SPIEGEL ZIET
-            // Reclame Macbook youtube #TODO
+            // Reclame Macbook youtube //TODO
             setContent(
                 "<video autoplay id='terug_in_de_tijd'>" +
                 "<source src='/video/terug_in_de_tijd.mp4' type='video/mp4'>" +
@@ -173,17 +170,20 @@ function render(sectie, direction) {
 
             //einde filmpje detecteren en dan fadeout
             document.getElementById('terug_in_de_tijd').addEventListener('ended', myHandler_trailer, false);
+
             function myHandler_trailer(e) {
                 fadeOut();
             }
             break;
 
+        case 10:
             // WAT HENNIE IN DE SPIEGEL ZIET
             // Wir war van vanalles (zie script)
-            #TODO
+            //TODO everything
 
             // WAT VDW IN DE SPIEGEL ZIET
             // Filmpje been met netkousen (wordt opgenomen op de filmdag)
+            //TODO set video
             setContent(
                 "<video autoplay id='terug_in_de_tijd'>" +
                 "<source src='/video/terug_in_de_tijd.mp4' type='video/mp4'>" +
@@ -191,51 +191,58 @@ function render(sectie, direction) {
             )
             fadeIn()
 
-            //einde filmpje detecteren en dan fadeout
+            // einde filmpje detecteren en dan fadeout
             document.getElementById('terug_in_de_tijd').addEventListener('ended', myHandler_trailer, false);
+
             function myHandler_trailer(e) {
                 fadeOut();
             }
             break;
 
+        case 10:
             // ACHTERGROND SORTEERSCENE
             // Grote eetzaal
-            #TODO find pic
-            setContent("<center><img src=/img/aeyels2.png alt='aeyels' style='width: 100vw; height: 100vh;'></center>")
+            //TODO find pic
+            setContent("<center><img src=/img/grote_eetzaal.png alt='grote_eetzaal' style='width: 100vw; height: 100vh;'></center>")
             fadeIn()
             break;
 
+        case 10:
             // ACHTERGROND LEYS ZIJN LES
             // Natuurkundig lokaal met blackbord
-            #TODO find pic
-            setContent("<center><img src=/img/aeyels2.png alt='aeyels' style='width: 100vw; height: 100vh;'></center>")
+            //TODO find pic
+            setContent("<center><img src=/img/lokaal_natuurkunde.png alt='lokaal_natuurkunde' style='width: 100vw; height: 100vh;'></center>")
             fadeIn()
             break;
 
+        case 10:
             // ACHTERGROND DHOEDT ZIJN LES
             // Computerzaal met blackbord
-            #TODO find pic
-            setContent("<center><img src=/img/aeyels2.png alt='aeyels' style='width: 100vw; height: 100vh;'></center>")
+            //TODO find pic
+            setContent("<center><img src=/img/lokaal_dhoedt.png alt='lokaal_dhoedt' style='width: 100vw; height: 100vh;'></center>")
             fadeIn()
             break;
 
+        case 10:
             // ACHTERGROND HENNIE HAAR LES
             // 3 draken (WOBA, ANA en MELA)
-            #TODO find pic
-            setContent("<center><img src=/img/aeyels2.png alt='aeyels' style='width: 100vw; height: 100vh;'></center>")
+            //TODO find pic
+            setContent("<center><img src=/img/mela.png alt='mela' style='width: 100vw; height: 100vh;'></center>")
             fadeIn()
             break;
 
+        case 10:
             // ACHTERGROND CURSUSVERKOOP
             // Ergens een gang in Zwijnaarde
-            #TODO find pic
+            //TODO find pic
             setContent("<center><img src=/img/aeyels2.png alt='aeyels' style='width: 100vw; height: 100vh;'></center>")
             fadeIn()
             break;
 
+        case 10:
             // FILMPJE TERUG IN DE TIJD
             // Eventueel filmpje film overpakken van Harry Potter Chamber of secrets + draaikolk
-            #TODO filmpje krijgen
+            //TODO filmpje krijgen
             setContent(
                 "<video autoplay id='terug_in_de_tijd'>" +
                 "<source src='/video/terug_in_de_tijd.mp4' type='video/mp4'>" +
@@ -245,15 +252,15 @@ function render(sectie, direction) {
 
             //einde filmpje detecteren en dan fadeout
             document.getElementById('terug_in_de_tijd').addEventListener('ended', myHandler_trailer, false);
+
             function myHandler_trailer(e) {
                 fadeOut();
             }
             break;
 
-
+        case 10:
             // ACHTERGROND TOVERPOORTSCENE
             // Deltaaaa  (eventueel hergebruik film vorig jaar).
-
             setContent(
                 "<video autoplay id='trailer'>" +
                 "<source src='/video/trailer.mp4' type='video/mp4'>" +
@@ -263,48 +270,58 @@ function render(sectie, direction) {
 
             //einde filmpje detecteren en dan fadeout
             document.getElementById('trailer').addEventListener('ended', myHandler_trailer, false);
+
             function myHandler_trailer(e) {
                 fadeOut();
             }
             break;
 
-            // ACHTERGROND BOSSCENE
+        case 10:
+            // ACHTERGROND BOS SCENE
             // Creepy donker bos met een vuurtje waarrond de acteurs kunnen zitten
-            #TODO foto vinden
-            #TODO vuur animeren
-
-            // ACHTERGROND LAPLACE TRANSFORMATIE COISE
-            // Lokaal in Plateau
-            #TODO find pic
-            setContent("<center><img src=/img/aeyels2.png alt='aeyels' style='width: 100vw; height: 100vh;'></center>")
+            //TODO foto vinden
+            //TODO vuur animeren
+            setContent("<center><img src=/img/kampvuur.png alt='kampvuur' style='width: 100vw; height: 100vh;'></center>")
             fadeIn()
             break;
 
-            // LAPLACE TRANSFORMATIE COISE
-            // Coise rukt hart uit lichaam #schaduw en steekt het in haar dagboek.
-            // Als ze dan het dagboek terug open doet staat er een afbeelding van het hart.
-            #TODO foto van het dagboek?
+        case 10:
+            // ACHTERGROND LAPLACE TRANSFORMATIE COISE
+            // Lokaal in Plateau
+            //TODO find pic
+            setContent("<center><img src=/img/lokaal_coise.png alt='lokaal_coise' style='width: 100vw; height: 100vh;'></center>")
+            fadeIn()
+            break;
 
+        case 10:
+            // LAPLACE TRANSFORMATIE COISE
+            // Coise rukt hart uit lichaam //schaduw en steekt het in haar dagboek.
+            // Als ze dan het dagboek terug open doet staat er een afbeelding van het hart.
+            //TODO foto van het dagboek?
+
+        case 10:
             // ACHTERGROND DOOD VAN LILLY
             // Woonkamer Lilly en Leys. Nadat Coisemort Harry heeft proberen vermoorden moet elk meubelstuk kapot zijn buiten de spiegel.
             // We dachten hiervoor echte meubelstukken op het podium te zetten dus zou de achtergrond vooral wat schilderijen of een vensterbank met bloemetjes moeten zijn.
-            #TODO meubels
-            #TODO find pic maybe 2 pics?
+            //TODO meubels
+            //TODO find pic maybe 2 pics?
             setContent("<center><img src=/img/aeyels2.png alt='aeyels' style='width: 100vw; height: 100vh;'></center>")
             fadeIn()
             break;
 
+        case 10:
             // ACHTERGROND EEN SPIEGELBEELD UIT HET VERLEDEN
             // Gang in Zwijnaarde
-            #TODO find pic
+            //TODO find pic
             setContent("<center><img src=/img/aeyels2.png alt='aeyels' style='width: 100vw; height: 100vh;'></center>")
             fadeIn()
             break;
 
+        case 10:
             // ACHTERGROND VECHTSCENE
             // Dezelfde achtergrond als bij de leraarskamer.
-            #TODO enter pic
-            setContent("<center><img src=/img/aeyels2.png alt='aeyels' style='width: 100vw; height: 100vh;'></center>")
+            //TODO enter pic
+            setContent("<center><img src=/img/leraarskamer.png alt='leraarskamer' style='width: 100vw; height: 100vh;'></center>")
             fadeIn()
             break;
 
--- routes/index.js
+++ routes/index.js
@@ -14,17 +14,4 @@ router.get('/show/:section?', function(req, res, next) {
     });
 });
 
-router.get('/timeline', function(req, res, next) {
-    res.render('timeline', {
-        title: 'Timeline',
-    });
-});
-
-router.get('/tinder', function(req, res, next) {
-    res.render('tinder', {
-        title: 'Tinder',
-    });
-});
-
-
 module.exports = router;
